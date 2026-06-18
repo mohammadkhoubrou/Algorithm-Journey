487ba7dbf97eb3bce64fcfd8df1797d96866a819
Mohammad Khoubrou <101056308+mohammadkhoubrou@users.noreply.github.com>
2026-06-18 19:34:03 +0330

    Add files via upload

---
+++ String Algorithms/Anagrams/anagrams.py
@@ -0,0 +1,12 @@
+def anagrams(S):
+    d = {}
+    S = {word for word in S.split()}
+    for word in S:
+        s = "".join(sorted(word)) 
+        if s in d:
+            d[s].append(word)
+        else:
+            d[s] = [word]
+    return [d[s] for s in d if len(d[s]) > 1]
+
+print(anagrams("below the car is a rat drinking cider and bending its elbow while this thing is an arc that can act like a cat which cried during the night caused by pain in its bowel"))
